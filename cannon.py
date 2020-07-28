b7b63212a94c58f9ea0c55dad24b7ba6b973166d
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-07-28 17:28:47 -0400

    Update cannon.py

--- cannon.py
+++ cannon.py
@@ -9,3 +9,4 @@ async def on_message(message):
                 if reaction.custom_emoji:
                     if reaction.emoji.name == content[1]:
                         await reaction.clear()
+client.run('id')
