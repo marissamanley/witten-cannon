514b0d87978d77deddd43c844fe6c4a443ee3110
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-07-28 17:26:43 -0400

    Update cannon.py

--- cannon.py
+++ cannon.py
@@ -4,7 +4,8 @@ client = discord.Client()
 async def on_message(message):
     content = message.content.split(" ")
     if content[0] == "-nuke":
-        if content[1][:2] == "<:":
-            async for message in message.channel.history(limit=200):
-                if content[1] in message.content:
-                    await message.delete()
+        async for message in message.channel.history(limit=int(content[2])):
+            for reaction in message.reactions:
+                if reaction.custom_emoji:
+                    if reaction.emoji.name == content[1]:
+                        await reaction.clear()
